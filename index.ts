cc1343d56ac0ddf007229cd8061d61a3c69427f4
rtardo <84642031+rtardo@users.noreply.github.com>
2023-07-17 22:41:42 +0300

    initialize the project

---
+++ index.ts
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { Client, Events, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+  ],
+});
+
+client.once(Events.ClientReady, c => {
+  console.log(`Ready! Logged in as ${c.user.tag }`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
